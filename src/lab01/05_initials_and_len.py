b630970ebb732a1373b048b622b2a75373973d45
arch160 <egorkarkhipov@yandex.ru>
2025-09-19 15:59:56 +0400

    Добавил

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,4 +1,4 @@
 an = input('Фамилия:')
 sn = input('Имя:')
 bn = input('Отчество:')
-print('ФИО:', an, sn, bn, 'Инициалы:', f'{an[0]}{sn[0]}{bn[0]} Длина:', len(an)+len(bn)+len(sn) )
+print('ФИО:', an, sn, bn, 'Инициалы:', f'{an[0]}{sn[0]}{bn[0]} Длина:', len(an)+len(bn)+len(sn)+2 )
